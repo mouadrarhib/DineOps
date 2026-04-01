f8005c9611c897853062b510eca8b3cf7f8528e0
MOUAD RARHIB <mouad159mouad@gmail.com>
2026-04-01 12:09:44 +0100

    add shared error code enum

---
+++ dineOps/src/main/java/com/mouad/dineops/dineOps/common/enums/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.mouad.dineops.dineOps.common.enums;
+
+public enum ErrorCode {
+	BAD_REQUEST,
+	UNAUTHORIZED,
+	FORBIDDEN,
+	NOT_FOUND,
+	CONFLICT,
+	VALIDATION_ERROR,
+	INTERNAL_SERVER_ERROR
+}
